d6832f3a18483944b662b86e18086f8d093316a1
Edgar Acosta <er.acosta@gmail.com>
2023-10-04 20:33:16 -0400

    Added Rotations

--- pointcp/design3/PointCP.java
+++ pointcp/design3/PointCP.java
@@ -9,5 +9,22 @@ abstract class PointCP {
 		this.xOrRho = xOrRho;
 		this.yOrTheta = yOrTheta;
 	}
+	/**
+   * Rotates the specified point by the specified number of degrees.
+   * Not required until E2.30
+   *
+   * @param point The point to rotate
+   * @param rotation The number of degrees to rotate the point.
+   * @return The rotated image of the original point.
+   */
+	public PointCP rotatePoint(double rotation){
+		double radRotation = Math.toRadians(rotation);
+		double X = getX();
+		double Y = getY();
+			
+		return new PointCP('C',
+		(Math.cos(radRotation) * X) - (Math.sin(radRotation) * Y),
+		(Math.sin(radRotation) * X) + (Math.cos(radRotation) * Y));
+	}
 
 }
--- pointcp/design5/PointCP.java
+++ pointcp/design5/PointCP.java
@@ -9,5 +9,31 @@ abstract class PointCP {
 		this.xOrRho = xOrRho;
 		this.yOrTheta = yOrTheta;
 	}
+	public double getDistance(PointCP pointB){
+		// Obtain differences in X and Y, sign is not important as these values
+		// will be squared later.
+		double deltaX = getX() - pointB.getX();
+		double deltaY = getY() - pointB.getY();
+		
+		return Math.sqrt((Math.pow(deltaX, 2) + Math.pow(deltaY, 2)));
+  	}
+
+  /**
+   * Rotates the specified point by the specified number of degrees.
+   * Not required until E2.30
+   *
+   * @param point The point to rotate
+   * @param rotation The number of degrees to rotate the point.
+   * @return The rotated image of the original point.
+   */
+	public PointCP rotatePoint(double rotation){
+		double radRotation = Math.toRadians(rotation);
+		double X = getX();
+		double Y = getY();
+			
+		return new PointCP('C',
+		(Math.cos(radRotation) * X) - (Math.sin(radRotation) * Y),
+		(Math.sin(radRotation) * X) + (Math.cos(radRotation) * Y));
+  }
 
 }
